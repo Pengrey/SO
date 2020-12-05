f70d11a7e8ab0688b905d6d94ca0314aad7fc820
Pedro-Lopes-Frisson <pdfl@ua.pt>
2020-12-05 16:49:57 +0000

    A

--- procstat.sh
+++ procstat.sh
@@ -8,10 +8,57 @@
 #sleep 2
 #awk '/rchar|wchar/{printf $0"\n"}' /proc/11052/io
 #ola=( $(cat /proc/$pid_s/comm) $(ls -alh /proc/ | grep "$pid_s" | awk ' { printf "%s\n",$3 }') $pid_s $(cat /proc/$pid_s/status | awk '/VmSize|VmRSS/ { printf $2"\n" }') $(cat /proc/$pid_s/io | awk '/rchar|wchar/ { printf $2"\n" }') $(ls -alh /proc/ | grep $pid_s | awk '{ printf  "\"%s %d %s\n\"",$6,$7,$8 }') ) &&  echo $ola
-function get_Info () {
-  pid_s=$1
-  ola+=( $(cat /proc/$pid_s/comm ; ls -alh /proc/ | grep $pid_s | awk ' {  printf "\t%s\n",$3 }'; printf "\t$pid_s"; awk '/VmSize|VmRSS/ { printf "\t"$2"\n" }' /proc/$pid_s/status  ;r1=$(awk '/rchar/ { printf "\t"$2"\n" }') /proc/$pid_s/io && echo $r1;w1=$(awk '/wchar/ { printf "\t"$2"\n" }') /proc/$pid_s/io && echo $w1  ; sleep $2 & proc=$!;wait $proc  ls -alh /proc/ | grep $pid_s | awk '{ printf  "\t%s %d %s\n",$6,$7,$8 }' | sed "s/\"//g") )
+
+###################################################
+##
+## get info com uma expresao regex
+## Usada para-> filtragem de nomes em comm
+## ou "*" para ir buscar todos os processos
+## parametros entrada:
+##      $1: numero de segundos
+##      $2: Expressao a utilizar
+##  parametros de saida:
+##      array de string com info
+###################################################
+function info_regex (){
+  process_info=( )
+  secs=$1
+  expression=$2
+  files=( $(grep $expression /proc/*/comm | awk -F ":" '{printf $1 "\n" }' | awk -F "/comm"  '{printf $1 "\n"}'))
+  for file in ${files[@]} ;do
+    rchar_i[$n]=$( awk '/rchar/ { printf $2 }' $file/io )
+    wchar_i[$n]=$( awk '/wchar/ { printf $2 }' $file/io )
+    sleep $secs & procs_ids[$n]=$!
+
+    n=$(( n + 1))
+  done
+
+  for pid in $(seq 0 $(( ${#procs_ids[@]} - 1 )));do
+# processo esta a correr ? sim espera
+#pode nao estar a correr para intervalos curtos
+    if [ -n "$(ps -p ${procs_ids[$pid]} -o pid=)" ];then
+      wait ${procs_ids[$pid]}
+    fi
+    number_id=$(echo ${files[$pid]} | awk -F "/proc/" '{printf $2}')
+    comm=( $(cat ${files[$pid]}/comm) )
+    user=( $(ls -lah /proc | grep $number_id | awk ' { printf "%s" , $3 }') )
+    mem=( $(awk '/VmSize/ { printf $2 }' ${files[$pid]}/status) )
+    rss=( $(awk '/VmRSS/ { printf $2 }' ${files[$pid]}/status) )
+    date_proc=$(date -r ${files[$pid]} "+%Y %b %d %H:%M")
+    raterchar_i=$(awk -v secs=2 -v inir=${rchar_i[$pid]} '/rchar/ { printf "%f",(($2 - inir)/secs) }' ${files[$pid]}/io )
+
+    ratewchar_i=$(awk -v secs=2 -v iniw=${wchar_i[$pid]} '/wchar/ { printf "%f",(($2 - iniw )/secs) }' ${files[$pid]}/io )
+            #com  usr  pid  mem rss rdb wdb rr    rw    date
+    process_info+=( $(printf "%-11s  %-7s  %-5d  %10d  %8d  %18d  %18d  %12.2f  %12.2f  %-17s\n" $comm $user $number_id $mem $rss ${rchar_i[$pid]} ${wchar_i[$pid]} $raterchar_i $ratewchar_i "$date_proc") )
+  done
 }
+
+
+
+
+
+
+################################################
 
 function usage () {
   echo "specify the number of seconds"
@@ -25,15 +72,12 @@ done
 [[ -v secsW ]] || usage
 
 while getopts ":c:s:e:u:p:mtdwr:" name ;do
-  echo "fck"
   case $name in
     [c]) # regular expression to get pid to analyse
-      echo "ola"
-      pids=$(pgrep "$OPTARG")
-      for p in ${pids[@]};do
-        get_Info $p $secsW
-      done;
-      echo ${ola[@]}
+      info_regex $secsW $OPTARG
+      for i in ${!process_info[@]};do
+        echo ${process_info[$i]}      #sim
+      done
       ;;
     [s]) # data minima do inicio do processo
       echo "nice"
@@ -45,8 +89,8 @@ while getopts ":c:s:e:u:p:mtdwr:" name ;do
       echo "nice"
       echo "$OPTARG";;
     [p]) # numero processos a analisar
-      echo "nice"
-      echo "$OPTARG";;
+      process_info=${process_info[@]:0:$OPTARG}
+      ;;
     [m]) # sort on mem
       echo "nice"
       echo "$OPTARG";;
